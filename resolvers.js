1f51e7e9c6bee611017290a901e375b98c40aea8
nattyait <nattanicha@gmail.com>
2019-09-04 00:25:29 +0700

    Resolver with argument and Query

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,18 @@
 const db = require('./db')
 const Query = {
    test: () => 'Test Success, GraphQL server is up & running !!',
-   employees:() => db.employees.list()
+   employees:() => db.employees.list(),
+   //resolver function for employeebyId
+   employeeById:(root,args,context,info) => {
+      //args will contain parameter passed in query
+      return db.employees.get(args.id);
+   }
 }
-module.exports = {Query}
+
+//for each single employee object returned,resolver is invoked
+const Employee = {
+   fullName:(root,args,context,info) => {
+      return root.firstName+":"+root.lastName
+   }
+}
+module.exports = {Query,Employee}
