7161921ec494b07bf4d1ba351a81b418345bfb06
nattyait <nattanicha@gmail.com>
2019-09-04 00:39:41 +0700

    Add folder ex4

---
+++ ex4/resolvers.js
@@ -0,0 +1,21 @@
+const db = require('./db')
+const Query = {
+   test: () => 'Test Success, GraphQL server is up & running !!',
+   employees:() => db.employees.list(),
+   //resolver function for employeebyId
+   employeeById:(root,args,context,info) => {
+      //args will contain parameter passed in query
+      return db.employees.get(args.id);
+   }
+}
+
+//for each single employee object returned,resolver is invoked
+const Employee = {
+   fullName:(root,args,context,info) => {
+      return root.firstName+":"+root.lastName
+   },
+   company:(root) => {
+      return db.companies.get(root.comId);
+   }
+}
+module.exports = {Query,Employee}
