eb06fe3a41ab1eee73e789844361078706a2f031
nattyait <nattanicha@gmail.com>
2019-09-04 00:04:35 +0700

    Ex.2

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,6 @@
+const db = require('./db')
 const Query = {
-   test: () => 'Test Success, GraphQL server is up & running !!'
+   test: () => 'Test Success, GraphQL server is up & running !!',
+   employees:() => db.employees.list()
 }
 module.exports = {Query}
