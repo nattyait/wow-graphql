f24f44b11f7da9016cce4f0995356b701181c103
nattyait <nattanicha@gmail.com>
2019-10-20 15:23:39 +0700

    Create Ex8 folder :baby:

---
+++ Ex8_react_integration/db.js
@@ -0,0 +1,8 @@
+const { DataStore } = require('notarealdb');
+
+const store = new DataStore('./data');
+
+module.exports = {
+   employees:store.collection('employees'),
+   companies:store.collection('companies')
+};
