2be9bd7f4bbc0bba9865335583d94bf84d717fb0
nattyait <nattanicha@gmail.com>
2019-10-20 15:37:49 +0700

    Modify index.js to directly call Graphql using gql :baby:

--- hello-world-client/src/index.js
+++ hello-world-client/src/index.js
@@ -1,12 +1,89 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// apollo client
+// with the change in this file, we can ignore App.js since we 
+// don't call the App dom here.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost'
+import gql from 'graphql-tag'
+
+const endPointUrl = 'http://localhost:9000/graphql'
+const client = new ApolloClient({
+   link: new HttpLink({uri:endPointUrl}),
+   cache:new InMemoryCache()
+});
+
+async function loadEmployeesAsync() {
+   const query = gql`
+   {
+      employees{
+         id
+         firstName
+         lastName
+         company{
+            name
+         }
+      }
+   }
+   `
+   const {data} = await client.query({query}) ;
+   return data.employees;
+}
+class  App  extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         employees:[]
+      }
+      this.employeeTemplate =  [];
+   }
+   async loadEmployees() {
+      const employeeData =  await loadEmployeesAsync();
+      this.setState({
+        employees: employeeData
+      })
+      console.log("loadEmployees")
+   }
+   render() {
+      return(
+         <div>
+            <input type = "button"  value = "loadEmployees" onClick = {this.loadEmployees.bind(this)}/>
+            <div>
+               <br/>
+               <hr/>
+               <table border = "3">
+                  <thead>
+                     <tr>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Company Name</td>
+                     </tr>
+                  </thead>
+                  
+                  <tbody>
+                     {
+                        this.state.employees.map(s => {
+                           return (
+                              <tr key = {s.id}>
+                                 <td>
+                                    {s.firstName}
+                                 </td>
+                                 <td>
+                                    {s.lastName}
+                                 </td>
+                                 <td>
+                                    {s.company.name}
+                                 </td>
+                              </tr>
+                           )
+                        })
+                     }
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      )
+   }
+}
+ReactDOM.render(<App/>, document.getElementById('root'));
